b9ddfc6d2dbc396cd93840d3c9f044a272ba788c
saschaeggi <sascha@amazeelabs.com>
2015-12-04 13:52:28 +0100

    INTZRH-133 stripped down version to drag&drop only

--- js/paragraphs.admin.js
+++ js/paragraphs.admin.js
@@ -6,29 +6,6 @@
    attach: function (context, settings) {
      paragraph_widget = '.field--widget-entity-reference-paragraphs'; /*Paragraph container*/
 
-    /*Set toggles*/
-     $(paragraph_widget + ' .field-multiple-table .field-label', context).each(function(){
-       if (!$(this).find('.paragraph-toggle').length) {
-         $(this).append('<a class="paragraph-toggle">Collapse</a>');
-       }
-     });
-
-     /*Toggle function*/
-     $(paragraph_widget +' .paragraph-toggle', context).each(function(e){
-       $(this).click(function(e){
-         e.preventDefault();
-         id = '#' + $(this).parents('table').attr('id');
-         draggable = id + ' > tbody > tr.draggable';
-         $(paragraph_widget).toggleClass('collapsed-items');
-         $(draggable).toggleClass('collapsed');
-         if ( $(this).text() == 'Collapse' ) {
-          $(this).text('Expand');
-         } else {
-           $(this).text('Collapse');
-         }
-       });
-     });
-
      /*Check if user is dragging*/
      $(paragraph_widget + ' .draggable .tabledrag-handle', context).mousedown(function(){
        id = '#' + $(this).parents('table').attr('id');
@@ -50,9 +27,7 @@
      /*Check if user is finished dragging*/
      if( $(paragraph_widget + ' .draggable').length ) {
        $('body', context).mouseup(function(){
-         if(!$(event.target).closest('.paragraph-toggle').length) {
-            $(paragraph_widget).not('.collapsed-items').find('.draggable').removeClass('collapsed');
-          }
+          $(paragraph_widget).not('.collapsed-items').find('.draggable').removeClass('collapsed');
 
           /*Scroll to*/
          if( $('#edit-paragraph', context).length ) {
